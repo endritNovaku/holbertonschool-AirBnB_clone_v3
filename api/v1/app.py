24cbc4b5540e0566466238f738061d6a04f967e9
KristiSeraj <serajkristi@gmail.com>
2022-09-15 12:58:02 -0700

    not found error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 Module starting the API
 """
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from os import getenv
 
@@ -20,6 +20,16 @@ app.register_blueprint(app_views)
 def close(self):
     """Clossing session method"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """
+    Error handler for 404 errors that returns a JSON-formatted
+    404 status code response
+    """
+    return make_response(jsonify({"error": "Not found"}), 404)
+
 
 if __name__ == "__main__":
     """
