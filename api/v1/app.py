b6e043730dde7628f0c79f1f527af363522ee585
KristiSeraj <serajkristi@gmail.com>
2022-09-15 08:44:13 -0700

    update __init

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,24 +3,20 @@
 Register a blueprint of app_view
 and uses a teardown method to close the storage
 """
-from os import getenv
-import models
+from os import environ
 from flask import Flask
+from models import storage
 from api.v1.views import app_views
-
-
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
-
 @app.teardown_appcontext
 def close(self):
     """Method to handle teardown appcontext"""
-    models.storage.close()
+    storage.close()
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     env_host = getenv('HBNB_API_HOST', default='0.0.0.0')
-    env_port = getenv('HBNB_API_PORT', default=5000)
+    env_port = getenv('HBNB_API_PORT', default='5000')
     app.run(host=env_host, port=env_port, threaded=True)
