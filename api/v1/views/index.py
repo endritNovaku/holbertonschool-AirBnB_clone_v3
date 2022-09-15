6b744fdaf00322c9f6852944238cd6f841edd483
KristiSeraj <serajkristi@gmail.com>
2022-09-15 12:44:27 -0700

    count objects

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,7 @@
 """ import app_views and create route to status """
 from api.v1.views import app_views
 from flask import jsonify, request
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
@@ -9,4 +10,18 @@ def get_status():
     """ return the status rout in json form """
     if request.method == 'GET':
         response = {"status": 'OK'}
+        return jsonify(response)
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    if request.method == 'GET':
+        response = {
+                "amenities": storage.count('Amenity'),
+                "cities": storage.count('City'),
+                "places": storage.count('Place'),
+                "reviews": storage.count('Review'),
+                "states": storage.count('State'),
+                "users": storage.count('User')
+                }
         return jsonify(response)
