24a6a2ceb0b404bd979b94aa87eab744aed140d2
KristiSeraj <serajkristi@gmail.com>
2022-09-15 04:49:06 -0700

    pycodestyle fixed

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""API modules needed"""
 from models import storage
 from api.v1.views import app_views
 from flask import Flask
@@ -9,10 +9,12 @@ from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def close(self):
     """Method to handle teardown appcontext"""
     storage.close()
+
 
 if __name__ == '__main__':
     env_host = getenv('HBNB_API_HOST')
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""Importing modules needed"""
 from api.v1.views import app_views
 from flask import jsonify
 
