64e5459a62dd7a3b595e2f580e029ef7b94c4ad6
KristiSeraj <serajkristi@gmail.com>
2022-09-17 13:07:15 -0700

    http acces control cors

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,9 +6,11 @@ from api.v1.views import app_views
 from flask import Flask, make_response, jsonify
 from models import storage
 from os import getenv
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 env_host = getenv('HBNB_API_HOST', '0.0.0.0')
 env_port = getenv('HBNB_API_PORT', 5000)
